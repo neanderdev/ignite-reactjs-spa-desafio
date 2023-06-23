6e3eed82ffd1114dd0afbab18c78cd5471ce6c34
Neander de Souza <neander.faria@hotmail.com>
2023-06-23 09:45:32 -0300

    chore: our coffees

---
+++ src/pages/Home/components/OurCoffees/index.tsx
@@ -0,0 +1,13 @@
+import { TitleText } from '../../../../components/Typography'
+
+import { OurCoffeesContainer } from './styles'
+
+export function OurCoffees() {
+  return (
+    <OurCoffeesContainer className="container">
+      <TitleText size="l" color="subtitle">
+        Nossos cafés
+      </TitleText>
+    </OurCoffeesContainer>
+  )
+}
