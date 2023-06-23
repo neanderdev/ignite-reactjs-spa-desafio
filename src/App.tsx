96b1360ca71cc241e59eeeecf73f42506744e079
Neander de Souza <neander.faria@hotmail.com>
2023-06-23 09:29:52 -0300

    refactor: styles global

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 export function App() {
-  return <h1>Coffe Delivery</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Welcome</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
